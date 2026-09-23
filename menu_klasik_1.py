c050571aa384eaaf1ea58cb197334fbcebcd4d10
mesalllnya <mifsalf05@gmail.com>
2026-09-23 20:22:52 +0700

    update caesar chiper

--- menu_klasik_1.py
+++ menu_klasik_1.py
@@ -14,9 +14,9 @@ def caesarChiper_enkripsi(plaintext: str, shift: int) -> str:
     Returns:
         str: Ciphertext Yang telah dienkripsi
     """
-
     cipherText = ""
     shift = int(shift)
+    print(f"\n[PROSES] Menggeser karakter sebanyak {shift} langkah ke kanan...")
     for karakter in plaintext:
         # huruf atau abjad
         # rumus enkripsi caesar chiper: (P + K) % 26 menyesuaikan dengan urutan ASCII
@@ -24,19 +24,26 @@ def caesarChiper_enkripsi(plaintext: str, shift: int) -> str:
             # ini yang huruf kapital
             if(karakter.isupper()):
                 chiper = ((ord(karakter) - ord('A') + shift) % 26) + ord('A')
-                cipherText += chr(chiper)
+                
             # ini yang huruf kecil
             else:
                 chiper = ((ord(karakter) - ord('a') + shift) % 26) + ord('a')
-                cipherText += chr(chiper)
+
+            # proses
+            print(f"{karakter} -> {chr(chiper)}")
+            cipherText += chr(chiper)
+
         # angka
         elif(karakter.isdigit()):
             chiper = ((int(karakter) + shift) % 10)
+
+            # proses 
+            print(f"{karakter} -> {str(chiper)}")
             cipherText += str(chiper)
+
         # karakter lain dibiarkan
         else:
             cipherText += karakter
-
 
     return cipherText
 
@@ -55,54 +62,62 @@ def caesarChiper_dekripsi(chipertext: str, shift: int) -> str:
 
     plainText = ""
     shift = int(shift)
-    for karakter in plainText:
+    print(f"\n[PROSES] Menggeser karakter sebanyak {shift} langkah ke kiri...")
+    for karakter in chipertext:
         # huruf
         # rumus enkripsi caesar chiper: (P + K) % 26 karena menggunakan ascii jadi
         if(karakter.isalpha()):
             # ini yang huruf kapital
             if(karakter.isupper()):
                 plain = ((ord(karakter) - ord('A') - shift) % 26) + ord('A')
-                plainText += chr(plain)
+                
             # ini yang huruf kecil
             else:
                 plain = ((ord(karakter) - ord('a') - shift) % 26) + ord('a')
-                plainText += chr(plain)
+
+            print(f"{karakter} -> {chr(plain)}")
+            plainText += chr(plain)
+
         # angka
         elif(karakter.isdigit()):
             plain = ((int(karakter) - shift) % 10)
+            print(f"{karakter} -> {str(plain)}")
             plainText += str(plain)
+           
         # karakter lain dibiarkan
         else:
             plainText += karakter
 
-
     return plainText
-    
 
 def jalankan():
     # Judul Program Nanti Diganti
     print("\n--- Algoritma KLASIK Caesar Chiper ---")
     teks = input("Masukkan teks (Plaintext/Ciphertext): ")
-    kunci = int(input("Masukkan kunci (geser berapa): "))
 
-    print("\nPilih Aksi:")
-    print("1. Enkripsi")
-    print("2. Dekripsi")
-    aksi = input("Pilihan (1/2): ")
+    while True:
+        kunci = input("Masukkan kunci (geser): ")
 
-    if aksi == "1":
-        hasil = caesarChiper_enkripsi(teks,kunci)
-        print(f"\n[PROSES] Menggeser karakter sebanyak {kunci} langkah ke kanan...")
-        # print("[PROSES] A -> D, B -> E...")
-        print(f"\n✅ Hasil Enkripsi: {hasil}")
+        if kunci.isdigit():
+            kunci=int(kunci)
+            print("\nPilih Aksi:")
+            print("1. Enkripsi")
+            print("2. Dekripsi")
+            aksi = input("Pilihan (1/2): ")
 
-    elif aksi == "2":
-        hasil = caesarChiper_dekripsi(teks,kunci)
-        print(f"\n[PROSES] Menggeser karakter sebanyak {kunci} langkah ke kiri...")
-        # print("[PROSES] D -> A, E -> B...")
-        print(f"\n✅ Hasil Dekripsi: {hasil}")
+            if aksi == "1" :
+                hasil = caesarChiper_enkripsi(teks,kunci)        
+                print(f"\n✅ Hasil Enkripsi: {hasil}")
 
-    else:
-        print("Aksi tidak dikenal. Batal.")
+            elif aksi == "2" :
+                hasil = caesarChiper_dekripsi(teks,kunci)        
+                print(f"\n✅ Hasil Dekripsi: {hasil}")
+
+            else:
+                print("Aksi tidak dikenal. Batal.")
+        
+            break
+        else:
+            print("\nKunci harus berupa angka")
 
     input("\nTekan Enter untuk kembali ke Menu Utama...")
