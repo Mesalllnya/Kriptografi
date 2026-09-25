c3424c5d7ae9c9e6fae5fa84fa415606bd4eb15e
Erlan Rifqi <erlanrifqi@gmail.com>
2026-09-25 14:49:06 +0700

    Implement RSA dan AES Algorithms

--- menu_modern_1.py
+++ menu_modern_1.py
@@ -1,13 +1,118 @@
 # Semua Algoritma Kudu punya ini
 def algoritmaKu(plainText: str) -> str:
     # Kalau udah dibikin fungsinya terus ketik " 3 kali biar bikin autodocstringnya kayak dibawah
+    """
+    Melakukan enkripsi plaintext menggunakan algoritma RSA.
+
+    Args:
+        plainText (str): Teks yang akan dienkripsi.
+
+    Returns:
+        str: Ciphertext hasil enkripsi RSA.
+    """
+
+    # RSA Enkripsi
     cipherText = ""
+
+    print("\nProses RSA Enkripsi\n")
+    print("[1]. Plaintext : ", plainText)
+
+    #konversi plaintext menjadi byte
+    plaintext_bytes = plainText.encode("utf-8")
+
+    print("[2]. Konversi ke nilai byte: ", list(plaintext_bytes))
+    print("[3]. Rumus RSA: C = M^e mod n")
+
+    hasil = []
+
+    for i, byte in enumerate(plaintext_bytes):
+        # RSA Enkripsi
+        cipher = pow(byte, e, n)
+        hasil.append(str(cipher))
+
+        print(
+            f"  Karakter ke-{ i + 1}: "
+            f"M = {byte} -> C = {cipher}"
+        )
+
+    # Ciphertextt berupa angka yang dipisahkan spasi
+    cipherText = " ".join(hasil)
+
+    print("[4] Ciphertext : ", cipherText)
+
     return cipherText
 
+def dekripsiRSA(cipherText: str) -> str:
+    """
+    Melakukan dekripsi ciphertext menggunakan algoritma RSA.
+
+    Args:
+        cipherText (str): Ciphertext berupa angka yang dipisahkan spasi.
+
+    Returns:
+        str: Plaintext hasil dekripsi RSA.
+    """
+
+    print("\n Proses RSA Dekripsi")
+    print("[1] Ciphertext : ", cipherText)
+
+    try:
+        cipher_blocks = [
+            int(x) for x in cipherText.split()
+        ]
+    except ValueError:
+        print("[ERROR] Ciphertext harus berupa angka!")
+        return ""
+
+    print("[2] Rumus RSA: M = C^d mod n")
+
+    hasil = []
+
+    for i, cipher in enumerate(cipher_blocks):
+
+        plain = pow(cipher, d, n)
+        hasil.append(plain)
+
+        print(
+            f"    Blok ke-{i + 1}: "
+            f"C = {cipher} → M = {plain}"
+        )
+
+    # Konversi kembali angka menjadi karakter
+    plaintext = bytes(hasil).decode("utf-8")
+
+    print("[3] Konversi byte :", hasil)
+    print("[4] Plaintext :", plaintext)
+
+    return plaintext
+
+# Kunci RSA 
+
+# Bil Prima
+p = 61
+q = 53
+
+# n = p × q
+n = p * q
+
+# phi (n) = (p - 1)(q - 1)
+phi = (p - 1) * (q - 1)
+
+# Public exponent
+e = 17
+
+# Private exponent
+d = pow(e, -1, phi)
 
 def jalankan():
     # Judul Program Nanti Diganti
-    print("\n--- Algoritma KLASIK 1 ---")
+    print("\n--- Algoritma MODERN 1 (RSA) ---")
+
+    # Menampilkan informasi key
+    print("\n[INFO RSA]")
+    print(f"Public Key  : (e={e}, n={n})")
+    print(f"Private Key : (d={d}, n={n})")
+
     teks = input("Masukkan teks (Plaintext/Ciphertext): ")
 
     print("\nPilih Aksi:")
@@ -16,20 +121,23 @@ def jalankan():
     aksi = input("Pilihan (1/2): ")
 
     if aksi == "1":
-        # Nanti Ini Yang diganti
-        # DUMMY ENGINE ENKRIPSI + TAMPILAN PROSES
-        print("\n[PROSES] Menggeser karakter sebanyak 3 langkah ke kanan...")
-        print("[PROSES] A -> D, B -> E...")
-        print(f"\n✅ Hasil Enkripsi: {teks}_ENCRYPTED_KLASIK1")
+        # RSA Engine Enkripsi + Proses
+
+        cipherText = algoritmaKu(teks)
+        print(f"\n✅ Hasil Enkripsi: {cipherText}")
 
     elif aksi == "2":
-        # Nanti Ini Yang diganti
-        # DUMMY ENGINE DEKRIPSI + TAMPILAN PROSES
-        print("\n[PROSES] Menggeser karakter sebanyak 3 langkah ke kiri...")
-        print("[PROSES] D -> A, E -> B...")
-        print(f"\n✅ Hasil Dekripsi: {teks}_DECRYPTED_KLASIK1")
+        # RSA Engine Dekripsi + Proses
+
+        plainText = dekripsiRSA(teks)
+
+        if plainText != "":
+            print(f"\n✅ Hasil Dekripsi: {teks}_DECRYPTED_KLASIK1")
 
     else:
         print("Aksi tidak dikenal. Batal.")
 
     input("\nTekan Enter untuk kembali ke Menu Utama...")
+
+if __name__ == "__main__":
+    jalankan()
--- menu_modern_2.py
+++ menu_modern_2.py
@@ -1,13 +1,104 @@
+from Crypto.Cipher import AES
+
+# AES 128 menggunakan key 16 bytes
+key = b"1234567890abcdef"
+
 # Semua Algoritma Kudu punya ini
 def algoritmaKu(plainText: str) -> str:
-    # Kalau udah dibikin fungsinya terus ketik " 3 kali biar bikin autodocstringnya kayak dibawah
-    cipherText = ""
+    """
+    Melakukan enkripsi plaintext menggunakan AES-128.
+
+    Args:
+        plainText (str): Teks yang akan dienkripsi.
+
+    Returns:
+        str: Ciphertext hasil enkripsi dalam format hexadecimal.
+    """
+
+    print("\nProses AES Enkripsi")
+    print("[1] Plaintext :", plainText)
+
+    # Konversi plaintext menjadi bytes
+    data = plainText.encode("utf-8")
+    print("[2] Plaintext Bytes :", list(data))
+
+    # Padding agar panjang data kelipatan 16 byte
+    padding = 16 - (len(data) % 16)
+    data = data + bytes([padding] * padding)
+
+    print("[3] Setelah Padding :", list(data))
+    print("[4] AES-128 Key :", key.decode())
+
+    # Membuat AES dengan mode ECB
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    # Enkripsi
+    encrypted = cipher.encrypt(data)
+
+    # Ubah ke heksadesimal agar mudah disimpan
+    cipherText = encrypted.hex().upper()
+    print("[5] Ciphertext Hex :", cipherText)
+
     return cipherText
 
+def dekripsiAES(cipherText: str) -> str:
+    """
+    Melakukan dekripsi ciphertext menggunakan AES-128.
+
+    Args:
+        cipherText (str): Ciphertext hexadecimal.
+
+    Returns:
+        str: Plaintext hasil dekripsi.
+    """
+
+    print("\n Proses AES Dekripsi")
+    print("[1] Ciphertext Hex :", cipherText)
+
+    try:
+        #Konversi heksadesimal menjadi bytes
+        encrypted = bytes.fromhex(cipherText)
+    except ValueError:
+        print("[ERROR] Ciphertext harus berupa heksadesimal!")
+        return ""
+
+    print("[2] Ciphertext Bytes :", list(encrypted))
+    print("[3] AES-128 Key : ", key.decode())
+
+    # Membuat AES dengan key yang sama
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    # Dekripsi
+    decrypted = cipher.decrypt(encrypted)
+
+    print("[4] Hasil Dekripsi dengan Padding :", list(decrypted))
+
+    try:
+        # Ambil jumlah padding dari byte terakhit
+        padding = decrypted[-1]
+
+        # Hapus padding
+        plaintext_bytes = decrypted[:-padding]
+
+        # Konversi kembali ke teks
+        plaintext = plaintext_bytes.decode("utf-8")
+
+    except (ValueError, UnicodeDecodeError):
+        print("[ERROR] Gagal melakukan dekripsi!")
+        return ""
+
+    print("[5] Plaintext:", plaintext)
+    return plaintext
 
 def jalankan():
     # Judul Program Nanti Diganti
-    print("\n--- Algoritma KLASIK 1 ---")
+    print("\n--- Algoritma Modern 2 (AES-128) ---")
+    print("\n[INFO AES]")
+    print("Jenis      : AES-128")
+    print("Key        :", key.decode())
+    print("Key Length : 128-bit")
+    print("Block Size : 128-bit")
+
     teks = input("Masukkan teks (Plaintext/Ciphertext): ")
 
     print("\nPilih Aksi:")
@@ -16,20 +107,21 @@ def jalankan():
     aksi = input("Pilihan (1/2): ")
 
     if aksi == "1":
-        # Nanti Ini Yang diganti
-        # DUMMY ENGINE ENKRIPSI + TAMPILAN PROSES
-        print("\n[PROSES] Menggeser karakter sebanyak 3 langkah ke kanan...")
-        print("[PROSES] A -> D, B -> E...")
-        print(f"\n✅ Hasil Enkripsi: {teks}_ENCRYPTED_KLASIK1")
+        # AES ENGINE ENKRIPSI + TAMPILAN PROSES
+        cipherText = algoritmaKu(teks)
+        print(f"\n✅ Hasil Enkripsi: {cipherText}")
 
     elif aksi == "2":
-        # Nanti Ini Yang diganti
-        # DUMMY ENGINE DEKRIPSI + TAMPILAN PROSES
-        print("\n[PROSES] Menggeser karakter sebanyak 3 langkah ke kiri...")
-        print("[PROSES] D -> A, E -> B...")
-        print(f"\n✅ Hasil Dekripsi: {teks}_DECRYPTED_KLASIK1")
+        # AES ENGINE DEKRIPSI + TAMPILAN PROSES
+        plainText = dekripsiAES(teks)
+
+        if plainText != "":
+            print(f"\n✅ Hasil Dekripsi: {plainText}")
 
     else:
         print("Aksi tidak dikenal. Batal.")
 
     input("\nTekan Enter untuk kembali ke Menu Utama...")
+
+if __name__ == "__main__":
+    jalankan()
