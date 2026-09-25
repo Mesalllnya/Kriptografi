43c0b4fc9b7efba7e06d1faecde8110c7eb29ea5
Rio Meidi Ataurrahman <riomeidi29@gmail.com>
2026-09-25 10:14:31 +0700

    Remove unused compiled Python files and update menu display in main.py
    and Make Menu Super Enkripsi

--- menu_super_enkripsi.py
+++ menu_super_enkripsi.py
@@ -1,3 +1,7 @@
+import menu_klasik_1 as mk1
+import menu_klasik_2 as mk2
+
+
 # Semua Algoritma Kudu punya ini
 def algoritmaKu(plainText: str) -> str:
     # Kalau udah dibikin fungsinya terus ketik " 3 kali biar bikin autodocstringnya kayak dibawah
@@ -6,30 +10,72 @@ def algoritmaKu(plainText: str) -> str:
 
 
 def jalankan():
-    # Judul Program Nanti Diganti
-    print("\n--- Algoritma KLASIK 1 ---")
-    teks = input("Masukkan teks (Plaintext/Ciphertext): ")
+    """_summary_
+    Menjalankan Algoritma Super yang menggabungkan
+    4 Algoritma Kriptografi:
 
-    print("\nPilih Aksi:")
-    print("1. Enkripsi")
-    print("2. Dekripsi")
-    aksi = input("Pilihan (1/2): ")
+    1. Caesar Cipher
+    2. Vigenere
+    3. Modern Erlan 1
+    4. Modern Erlan 2
 
-    if aksi == "1":
-        # Nanti Ini Yang diganti
-        # DUMMY ENGINE ENKRIPSI + TAMPILAN PROSES
-        print("\n[PROSES] Menggeser karakter sebanyak 3 langkah ke kanan...")
-        print("[PROSES] A -> D, B -> E...")
-        print(f"\n✅ Hasil Enkripsi: {teks}_ENCRYPTED_KLASIK1")
+    """
+    print("\n--- Algoritma Super ---")
+    teks = input("Masukkan teks (Plaintext/Ciphertext) \nKHUSUS SUPERENRKRIPSI: ")
 
-    elif aksi == "2":
-        # Nanti Ini Yang diganti
-        # DUMMY ENGINE DEKRIPSI + TAMPILAN PROSES
-        print("\n[PROSES] Menggeser karakter sebanyak 3 langkah ke kiri...")
-        print("[PROSES] D -> A, E -> B...")
-        print(f"\n✅ Hasil Dekripsi: {teks}_DECRYPTED_KLASIK1")
+    while True:
+        print("\nPilih Aksi:")
+        print("1. Enkripsi")
+        print("2. Dekripsi")
+        aksi = input("Pilihan (1/2): ")
 
-    else:
-        print("Aksi tidak dikenal. Batal.")
+        if aksi == "1":
+            # 1. Mengenkripsi Menggunakan Caesar Cipher terlebih dahulu
+            kunci = input("Masukkan kunci (geser): ")
+            while not kunci.isdigit():
+                kunci = input("Masukkan kunci (geser): ")
+            kunci = int(kunci)
+            hasilCaesar = mk1.caesarChiper_enkripsi(teks, kunci)
+            mk1.proses_caesar_enkripsi(teks, kunci)
+
+            # 2. Hasil Caesar Cipher digunakan untuk Vignere
+            kunci = input("Masukkan kunci (Huruf): ")
+            hasilVigenere = mk2.vegenere_enkripsi(hasilCaesar, kunci)
+            mk2.proses_vegenere_enkripsi(hasilCaesar, kunci)
+
+            # 3. Hasil Vignere digunakan untuk Modern 1
+
+            # 4. Hasil modern 1 digunakan untuk Modern 2
+
+            # Print Hasil Enkripsi
+            print("Hasil Enkripsi: ", hasilVigenere)
+
+        elif aksi == "2":
+            # 1. Deskripsi dimulai dari modern 2
+
+            # 2. Deskripsi Modern 1
+            hasilModern1 = "apalah"
+
+            # 3. Deskripsi Vigenere
+            # FIXME - Sementara kuganti teks biar bisa ngetest
+            kunci = input("Masukkan kunci (Huruf): ")
+            hasilVigenere = mk2.vegenere_dekripsi(teks, kunci)
+            mk2.proses_vegenere_dekripsi(teks, kunci)
+
+            # 4. Deskripsi Caesar (Final)
+            kunci = input("Masukkan kunci (geser): ")
+            while not kunci.isdigit():
+                kunci = input("Masukkan kunci (geser): ")
+            kunci = int(kunci)
+            hasilCaesar = mk1.caesarChiper_dekripsi(hasilVigenere, kunci)
+            mk1.proses_caesar_dekripsi(hasilVigenere, kunci)
+
+            # Print Hasil Deskripsi
+            print("Hasil Deskripsi: ", hasilVigenere)
+
+        else:
+            print("Aksi tidak dikenal. Batal.")
+
+        break
 
     input("\nTekan Enter untuk kembali ke Menu Utama...")
